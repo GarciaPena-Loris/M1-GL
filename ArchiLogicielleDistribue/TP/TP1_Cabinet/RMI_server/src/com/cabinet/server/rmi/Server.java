01f0c662b8e60d3c97a736725e2fa585e29b9898
GarciaPena-Loris <loris.garcia-pena@etu.umontpellier.fr>
2023-10-16 00:20:41 +0200

    fin du rapport

--- ArchiLogicielleDistribue/TP/TP1_Cabinet/RMI_server/src/com/cabinet/server/rmi/Server.java
+++ ArchiLogicielleDistribue/TP/TP1_Cabinet/RMI_server/src/com/cabinet/server/rmi/Server.java
@@ -136,10 +136,8 @@ public class Server {
 
 			// Codebase
 			System.setProperty("java.rmi.server.codebase",
-					"file:///F:/DossierLoris/MonProfil/MesDocumentEcole/Fac/M1/M1-GL/ArchiLogicielleDistribue/TP/TP1_Cabinet/RMI_client/codebase/");
+					"file:///chemin_vers_projet/TP1_Cabinet/RMI_client/codebase/");
 			
-			// System.setProperty("java.rmi.server.codebase",
-			// 		"file:///C:/Users/loloy/Desktop/cours/M1/M1-GL/ArchiLogicielleDistribue/TP/TP1_Cabinet/RMI_client/codebase/");
 
 			CabinetMedical cabinet = new CabinetMedical();
 
