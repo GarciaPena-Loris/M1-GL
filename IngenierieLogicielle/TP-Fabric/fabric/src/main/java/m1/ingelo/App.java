07b729d0b4160beefc964d8de68307de81f95c45
Garcia-Pena Loris <loris.garcia-pena@etu.umontpellier.fr>
2023-11-15 09:40:21 +0100

    Debut fabric

---
+++ IngenierieLogicielle/TP-Fabric/fabric/src/main/java/m1/ingelo/App.java
@@ -0,0 +1,13 @@
+package m1.ingelo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
